d93988ef0a88056be620c66ee66750e1f023a149
Dmytro Spidchenko <spidchenko.d@gmail.com>
2025-02-05 16:05:53 +0100

    Increase default notification sound volume to 1

--- app/src/main/java/d/spidchenko/stars2d/util/SoundEngine.kt
+++ app/src/main/java/d/spidchenko/stars2d/util/SoundEngine.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.media.AudioAttributes
 import android.media.SoundPool
 
+private const val DEFAULT_VOLUME = 1F
+
 class SoundEngine(context: Context) {
     private val soundPool: SoundPool
     private val popId: Int
-    private val volume = 0.3F
 
     init {
         val audioAttributes = AudioAttributes.Builder()
@@ -15,23 +16,21 @@ class SoundEngine(context: Context) {
             .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
             .build()
         soundPool = SoundPool.Builder()
-            .setMaxStreams(5)
+            .setMaxStreams(1)
             .setAudioAttributes(audioAttributes)
             .build()
 
         val assetManager = context.assets
-        val descriptor = assetManager.openFd("bubble-pop.ogg")
+        val descriptor = assetManager.openFd("dingdong.ogg")
         popId = soundPool.load(descriptor, 0)
     }
 
-    fun playPop() = playSoundById(popId)
+    fun playDingDong() = playSoundById(popId)
 
     private fun playSoundById(soundId: Int) {
-        val leftVolume = volume
-        val rightVolume = volume
         val priority = 0
         val loopMode = 0
         val rate = 1F
-        soundPool.play(soundId, leftVolume, rightVolume, priority, loopMode, rate)
+        soundPool.play(soundId, DEFAULT_VOLUME, DEFAULT_VOLUME, priority, loopMode, rate)
     }
 }
